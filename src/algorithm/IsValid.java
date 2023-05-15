26c532d256097f34c196ddf1e70bb24b0ecfd4d1
chenruizhou1 <1259522407@qq.com>
2023-05-15 09:52:47 +0800

    C

---
+++ src/algorithm/IsValid.java
@@ -0,0 +1,47 @@
+package algorithm;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 1、左括号必须用相同类型的右括号闭合。
+ * 2、左括号必须以正确的顺序闭合。
+ * 3、每个右括号都有一个对应的相同类型的左括号。
+ */
+public class IsValid {
+
+    public static boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[');
+        }};
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || stack.peek() != map.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+
+}
